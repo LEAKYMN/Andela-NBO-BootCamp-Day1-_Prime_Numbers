112322d3674dd966e4c659327de9b706d804b650
LEAKYMN <leakymn@yahoo.com>
2017-06-12 22:25:23 +0300

    A new commit

--- asymptotic_analysis_prime_number.py
+++ asymptotic_analysis_prime_number.py
@@ -1,8 +1,29 @@
+Test 2 with for primenumsize in range(1, 25, 5):
+
 size: 1 time: 0.000000
 size: 6 time: 0.000000
 size: 11 time: 0.000000
 size: 16 time: 0.000000
 size: 21 time: 0.000000
 
+
+Process finished with exit code 0
+
+Test 2 with for primenumsize in range(1, 1000, 100):
+
+C:\Users\Owner\Desktop\andela_bootcamp_activity>python prime_number.py
+[2, 3, 5, 7, 11, 13, 17, 19, 23]
+size: 1 time: 0.000000
+size: 101 time: 0.000992
+size: 201 time: 0.001665
+size: 301 time: 0.014960
+size: 401 time: 0.007205
+size: 501 time: 0.015633
+size: 601 time: 0.031240
+size: 701 time: 0.031253
+size: 801 time: 0.031253
+size: 901 time: 0.046875
+
+C:\Users\Owner\Desktop\andela_bootcamp_activity>
 
 Process finished with exit code 0
